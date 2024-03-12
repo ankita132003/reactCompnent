714a7a371a86a84ebb0fd8cb9d30b1092b3af744
Shivam singh <shivam2003sy@gmail.com>
2024-03-12 22:24:48 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import Post from './component/Post';
-
+import {useDispatch, useSelector} from 'react-redux';
 function App() {
+  const dispatch = useDispatch();
+  const posts = useSelector(state => state.post.posts);
+
+  const addPost = (text) => {
+    console.log(text);
+    dispatch({type: 'ADD_POST', payload: text});
+
+  }
+
   return (
-   <Post/>
+    <>
+   <Post addPost={addPost}/>
+   <div>
+      {
+        posts.map((post, index) => {
+          return (
+            <div key={index}>
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          )
+        })
+      }
+   </div>
+   </>
   );
 }
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,8 +5,7 @@ import post from './postReducer'
 // import auth from './authReducer'
 // import socketReducer from './socketReducer'
 const rootReducer = combineReducers({
-    // Add your reducers here
-    post:post,
+  post : post
   });
   
   export default rootReducer;
